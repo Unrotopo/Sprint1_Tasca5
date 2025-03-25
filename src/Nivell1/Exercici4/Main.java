b002ef7d6cf5d28d106a23e5fd8dc2601966077a
Luis Portas <otrorotopo@gmail.com>
2025-03-25 10:54:19 +0100

    na

--- src/Nivell1/Exercici4/Main.java
+++ src/Nivell1/Exercici4/Main.java
@@ -1,10 +1,13 @@
 package Nivell1.Exercici4;
 
+import Nivell1.Exercici4.filemanagers.ReadFile;
+import Nivell1.Exercici4.filemanagers.WriteFile;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // WriteFile.runWriter();
-        // ReadFile.runFileReader();
+        WriteFile.runWriter();
+        ReadFile.runFileReader();
     }
 }
