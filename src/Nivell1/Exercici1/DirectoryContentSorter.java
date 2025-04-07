650dab1dc8265d358caef4d8a90ae2f5614ca3a5
Luis Portas <otrorotopo@gmail.com>
2025-04-07 12:07:38 +0200

    Se gestionan posibles excepciones en N1E1

--- src/Nivell1/Exercici1/DirectoryContentSorter.java
+++ src/Nivell1/Exercici1/DirectoryContentSorter.java
@@ -1,18 +1,32 @@
 package Nivell1.Exercici1;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class DirectoryContentSorter {
 
     public static void showSortedFiles(String path) {
 
-        File file = new File(path);
-        String[] list = file.list();
+        try {
+            File file = new File(path);
 
-        Arrays.sort(list);
-        for (String s : list) {
-            System.out.println(s);
+            if (!file.exists()) {
+                throw new IOException("The specified path does not exist.");
+            }
+
+            String[] list = file.list();
+            if (list == null) {
+                throw new ArrayIndexOutOfBoundsException("No files found in the directory.");
+            }
+
+            Arrays.sort(list);
+            for (String s : list) {
+                System.out.println(s);
+            }
+
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
