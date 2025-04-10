652e9430af90ff546eff5b186f68b9331377f0f2
Luis Portas <otrorotopo@gmail.com>
2025-04-10 12:40:37 +0200

    Nivel 1, ej.2. método showMoreSortedFiles refactorizado

--- src/Nivell1/Exercici2/TreeDirectoryContentSorter.java
+++ src/Nivell1/Exercici2/TreeDirectoryContentSorter.java
@@ -13,41 +13,56 @@ import java.util.List;
 public class TreeDirectoryContentSorter {
 
     public void showMoreSortedFiles(Path path) {
+        if (!Files.isDirectory(path)) {
+            return;
+        }
+
         try {
-            if (Files.isDirectory(path)) {
-                List<Path> children = new ArrayList<>();
-
-                try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                    for (Path child : directoryStream) {
-                        children.add(child);
-                    }
-                }
-
-                if (children.isEmpty()) {
-                    return;
-                }
-
-                Collections.sort(children);
-
-                System.out.println("\nContents of: \\" + path.getFileName());
-                for (Path child : children) {
-                    BasicFileAttributes attr = Files.readAttributes(child, BasicFileAttributes.class);
-                    if (Files.isDirectory(child)) {
-                        System.out.println("\t" + child.getFileName() + " -- D");
-                        System.out.println("\t\t[last modified: " + attr.lastModifiedTime() + "]");
-                    } else {
-                        System.out.println("\t" + child.getFileName() + " -- F");
-                        System.out.println("\t\t[last modified: " + attr.lastModifiedTime() + "]");
-                    }
-                }
-                for (Path child : children) {
-                    if (Files.isDirectory(child) && !Files.isSymbolicLink(child)) {
-                        showMoreSortedFiles(child);
-                    }
-                }
+            List<Path> children = getDirectoryChildren(path);
+            if (children.isEmpty()) {
+                return;
             }
+            processChildren(children, path);
+
         } catch(IOException e){
             System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Path> getDirectoryChildren(Path path) throws IOException {
+        List<Path> children = new ArrayList<>();
+
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+            for (Path child : directoryStream) {
+                children.add(child);
+            }
+        }
+        return children;
+    }
+
+    public void printFiles(List<Path> children, Path path) throws IOException {
+
+        System.out.println("\nContents of: \\" + path.getFileName());
+        for (Path child : children) {
+            BasicFileAttributes attr = Files.readAttributes(child, BasicFileAttributes.class);
+            if (Files.isDirectory(child)) {
+                System.out.println("\t" + child.getFileName() + " -- D");
+                System.out.println("\t\t[last modified: " + attr.lastModifiedTime() + "]");
+            } else {
+                System.out.println("\t" + child.getFileName() + " -- F");
+                System.out.println("\t\t[last modified: " + attr.lastModifiedTime() + "]");
+            }
+        }
+    }
+
+    public void processChildren(List<Path> children, Path path) throws IOException {
+        Collections.sort(children);
+        printFiles(children, path);
+
+        for (Path child : children) {
+            if (Files.isDirectory(child) && !Files.isSymbolicLink(child)) {
+                showMoreSortedFiles(child);
+            }
         }
     }
 }
